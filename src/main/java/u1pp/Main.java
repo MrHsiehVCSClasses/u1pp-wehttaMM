0325bf60ad0ca024bcc3fc964961d20f0703ef2c
wehttaMM <goudanli5174@gmail.com>
2023-08-25 15:10:38 +0800

    test commit

--- src/main/java/u1pp/Main.java
+++ src/main/java/u1pp/Main.java
@@ -11,6 +11,7 @@ public class Main
         System.out.println("Hello, Mr.Hsieh");
         printGreeting("Nerd");
         printGreeting("Mr.Hsieh");
+
         // Use this space to test your code, or actually run your project
         System.out.println("\n\nHi! This is an empty project.\nYour code will start running from Main.java.\nIt may be daunting to start a project from scratch, but try to have fun with it! :)\n");
     }
